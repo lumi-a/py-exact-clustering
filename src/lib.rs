35ded3767f1f0160dc029a3ea4898d9e4ee8f9fe
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-03-30 18:55:40 +0200

    Add simple bindings

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,61 @@
-use pyo3::prelude::*;
+use exact_clustering::{Cost, Discrete, KMeans, Point, WeightedKMeans, WeightedPoint};
+use ndarray::prelude::*;
+use pyo3::{exceptions::PyValueError, prelude::*};
 
-/// Formats the sum of two numbers as string.
+type PyPoints = Vec<Vec<f64>>;
+type PyWeightedPoints = Vec<(f64, Vec<f64>)>;
+
+fn to_points(points: PyPoints) -> Vec<Point> {
+    points.into_iter().map(Array1::from_vec).collect()
+}
+
+fn to_weighted_points(weighted_points: PyWeightedPoints) -> Vec<WeightedPoint> {
+    weighted_points
+        .into_iter()
+        .map(|(w, v)| (w, Array1::from_vec(v)))
+        .collect()
+}
+
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn price_of_kmeans_greedy(points: PyPoints) -> PyResult<f64> {
+    Ok(KMeans::new(&to_points(points))
+        .map_err(|e| PyValueError::new_err(e.to_string()))?
+        .price_of_greedy()
+        .0)
+}
+
+#[pyfunction]
+fn price_of_weighted_kmeans_greedy(weighted_points: PyWeightedPoints) -> PyResult<f64> {
+    Ok(WeightedKMeans::new(&to_weighted_points(weighted_points))
+        .map_err(|e| PyValueError::new_err(e.to_string()))?
+        .price_of_greedy()
+        .0)
+}
+
+#[pyfunction]
+fn price_of_kmedian_hierarchy(points: PyPoints) -> PyResult<f64> {
+    Ok(Discrete::kmedian(&to_points(points))
+        .map_err(|e| PyValueError::new_err(e.to_string()))?
+        .price_of_hierarchy()
+        .0)
+}
+
+#[pyfunction]
+fn price_of_weighted_kmedian_hierarchy(weighted_points: PyWeightedPoints) -> PyResult<f64> {
+    Ok(
+        Discrete::weighted_kmedian(&to_weighted_points(weighted_points))
+            .map_err(|e| PyValueError::new_err(e.to_string()))?
+            .price_of_hierarchy()
+            .0,
+    )
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn exact_clustering(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+fn clustering_rs(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(price_of_kmeans_greedy, m)?)?;
+    m.add_function(wrap_pyfunction!(price_of_weighted_kmeans_greedy, m)?)?;
+    m.add_function(wrap_pyfunction!(price_of_kmedian_hierarchy, m)?)?;
+    m.add_function(wrap_pyfunction!(price_of_weighted_kmedian_hierarchy, m)?)?;
     Ok(())
 }
